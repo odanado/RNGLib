1e7bc329c6f358e1c4b210aa696b15a06f763737
odan <odan3240@gmail.com>
2014-10-27 00:22:37 +0900

    Timer0のsetを忘れていたので追加

--- Parameters5Gen.hpp
+++ Parameters5Gen.hpp
@@ -64,7 +64,7 @@ public:
     }
 
     void set_key(uint32_t key_) { key = key_; }
-    void set_timer0(uint32_t Timer0_) { Timer0 = Timer0_; }
+    void set_timer0(uint32_t tt) { Timer0 = tt; }
 
 
 };
