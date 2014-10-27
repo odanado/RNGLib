6fafaf402b0bfaf3ab0cbe1c858d54accd122fb5
odan <odan3240@gmail.com>
2014-10-27 21:04:01 +0900

    MTを追加

---
+++ RNGLib/MT.hpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2014 odan
+ * License: MIT License
+ */
+
+#ifndef INCLUDED_MT_HPP
+#define INCLUDED_MT_HPP
+#include <random>
+namespace RNGLib {
+    typedef std::mt19937 mt;
+} // end RNGLib
+#endif
